b8ddfb7396272558f4276ae22b68b1d79630e3ac
Nishant Taneja <nishant.taneja2701@gmail.com>
2022-01-04 23:39:41 +0530

    initial commit

--- Sources/ProgressShimmerAnimation/ProgressShimmerAnimation.swift
+++ Sources/ProgressShimmerAnimation/ProgressShimmerAnimation.swift
@@ -1,6 +1,64 @@
-public struct ProgressShimmerAnimation {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
+public protocol ProgressShimmerAnimation: UIView {
+    
+    var progressShimmerAnimationLayer: CAGradientLayer? { get set }
+    var isAnimating: Bool { get set }
+    
+    func startProgressShimmerAnimation()
+    func stopProgressShimmerAnimation()
+    
+}
+
+extension ProgressShimmerAnimation {
+    
+    var progressShimmerAnimationLayer: CAGradientLayer? {
+        nil
     }
+    
+    var isAnimating: Bool {
+        false
+    }
+    
+    private func getGradientLayer() -> CAGradientLayer {
+        let layer = CAGradientLayer()
+        layer.frame = bounds
+        layer.startPoint = .zero
+        layer.endPoint = .init(x: 1, y: .zero)
+        let gradientColor1: CGColor = UIColor.init(white: 1, alpha: 0.2).cgColor
+        let gradientColor2: CGColor = UIColor.init(white: 1, alpha: 0.8).cgColor
+        layer.colors = [gradientColor1, gradientColor2, gradientColor1]
+        layer.locations = [0, 0.5, 1]
+        progressShimmerAnimationLayer = layer
+        return layer
+    }
+    
+    private func getShimmerAnimation() -> CABasicAnimation {
+        let animation = CABasicAnimation(keyPath: "locations")
+        animation.fromValue = [-1, -0.5, .zero]
+        animation.toValue = [1, 1.5, 2]
+        animation.repeatCount = .infinity
+        animation.duration = 1.5
+        return animation
+    }
+    
+    func startProgressShimmerAnimation() {
+        DispatchQueue.main.async {
+            self.isHidden = false
+            self.isAnimating =  true
+            self.layer.addSublayer(self.progressShimmerAnimationLayer ?? self.getGradientLayer())
+            let shimmerAnimation = self.getShimmerAnimation()
+            self.progressShimmerAnimationLayer?.add(shimmerAnimation, forKey: shimmerAnimation.keyPath)
+        }
+    }
+    
+    func stopProgressShimmerAnimation() {
+        DispatchQueue.main.async {
+            self.isHidden = true
+            self.isAnimating = false
+            self.progressShimmerAnimationLayer?.removeFromSuperlayer()
+            self.progressShimmerAnimationLayer = nil
+        }
+    }
+    
 }
